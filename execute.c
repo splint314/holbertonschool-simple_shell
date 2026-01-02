a036ce26e4abe3dd786b8d0a7591ac437233c0a3
aourzik <a.ourzik@gmail.com>
2026-01-02 12:41:00 +0100

    fix: modify ma execute command because my shell shows the files but doesn't execute

--- execute.c
+++ execute.c
@@ -1,32 +1,83 @@
 #include "shell.h"
 
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
+
+/**
+ * split_line - Splits a command line into arguments
+ * @line: The string to be split
+ *
+ * This function parses the string `line` and splits it into tokens
+ * separated by spaces, tabs, carriage returns, or alerts
+ * (as defined by TOK_DELIM). It dynamically allocates an array of
+ * pointers to the tokens and returns it. The last element of the
+ * array is always NULL to indicate the end.
+ *
+ * Return: An array of strings (tokens), or exits the program if
+ * memory allocation fails.
+ */
+char **split_line(char *line)
+{
+	int bufsize = TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char *));
+	char *token;
+
+	if (!tokens)
+		exit(1);
+
+	token = strtok(line, TOK_DELIM);
+	while (token)
+	{
+		tokens[position++] = token;
+
+		if (position >= bufsize)
+		{
+			bufsize += TOK_BUFSIZE;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+				exit(1);
+		}
+
+		token = strtok(NULL, TOK_DELIM);
+	}
+	tokens[position] = NULL;
+	return tokens;
+}
+
 /**
  * execute_command - executes a single command
  * @command: command to execute (absolute path)
  */
-void execute_command(char *command)
+void execute_command(char *line)
 {
 	pid_t pid;
 	int status;
-	char *argv[2];
-	char *path;
+	char **args;
 
-	path = resolve_path(command);
-
-	argv[0] = path;
-	argv[1] = NULL;
+	args = split_line(line);
+	if (!args[0])
+	{
+		free(args);
+		return;
+	}
 
 	pid = fork();
-	if (pid == -1)
-		return;
-
 	if (pid == 0)
 	{
-		if (execve(path, argv, environ) == -1)
-		{
-			_exit(127);
-		}
+		if (execve(args[0], args, NULL) == -1)
+			exit(1);
+	}
+	else if (pid < 0)
+	{
+		exit(1);
 	}
 	else
-		waitpid(pid, &status, 0);
+	{
+		do
+		{
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	free(args);
 }
